9f5096925b7570eaf78a87c6b0ed464a8b27f68b
Gree <ysm0759@naver.com>
2021-08-05 22:36:13 +0900

    됐다

--- SuperMario/src/SuperMario/MainMario.java
+++ SuperMario/src/SuperMario/MainMario.java
@@ -1,10 +1,15 @@
 package SuperMario;
 
+import java.util.Scanner;
+
 public class MainMario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String a;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextLine();
+		System.out.print(a);
 	}
 
 }
