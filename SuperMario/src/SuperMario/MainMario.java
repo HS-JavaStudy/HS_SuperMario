ed2d61ec187d19acd82b3ddc6dbb3052a9d099b3
ujkkk <jean207@naver.com>
2021-08-05 23:04:55 +0900

    imges 폴더와 mario.class 추가

--- SuperMario/src/SuperMario/MainMario.java
+++ SuperMario/src/SuperMario/MainMario.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 public class MainMario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String a;
-		Scanner sc = new Scanner(System.in);
-		a = sc.nextLine();
-		System.out.print(a);
+	
+		new Mario();
+		
 	}
-
 }
---
+++ SuperMario/src/SuperMario/Mario.java
@@ -0,0 +1,104 @@
+package SuperMario;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+
+public class Mario extends JFrame implements KeyListener {
+
+	Image image;
+	int x = 100, y = 100, sel = 1;
+
+	public Mario() {
+		super("마리오"); // 프로그램 명
+		setLayout(null); // 레이아웃 매니저 사용 안함
+		setResizable(false); // 창 크기 고정
+		setSize(1000, 1000);// 창 크기 지정
+		setBackground(Color.WHITE); // 창 배경 색상 지정
+		setVisible(true);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램 닫힘 설정
+
+		addKeyListener(this); // frame에 KeyListener 장착
+
+		x = (int) getWidth() / 2; // frame의 너비 얻기
+		y = (int) getHeight() / 2;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_NUMPAD6 || // 숫자키 NUMPAD
+				key == KeyEvent.VK_KP_RIGHT) {
+			sel = (sel == 1) ? 2 : 1; // 삼항연산자
+			x = (x < getWidth()) ? x + 10 : -image.getWidth(this);
+		} else {
+			int key1 = e.getKeyCode();
+			if (key1 == KeyEvent.VK_LEFT || key1 == KeyEvent.VK_NUMPAD4 || // 숫자키 NUMPAD
+					key1 == KeyEvent.VK_KP_LEFT) {
+				sel = (sel == 1) ? 3 : 1; // 삼항연산자
+				x = (x > 0) ? x - 10 : getWidth() + image.getWidth(this);
+			}
+			int key2 = e.getKeyCode();
+			if (key2 == KeyEvent.VK_UP || key2 == KeyEvent.VK_NUMPAD8 || // 숫자키 NUMPAD
+					key2 == KeyEvent.VK_KP_UP) {
+				sel = (sel == 1) ? 4 : 1; // 삼항연산자
+				y = (y > 0) ? y - 10 : getHeight() + image.getHeight(this);
+
+			}
+			int key3 = e.getKeyCode();
+			if (key3 == KeyEvent.VK_DOWN || key3 == KeyEvent.VK_NUMPAD2 || // 숫자키 NUMPAD
+					key3 == KeyEvent.VK_KP_DOWN) {
+				sel = (sel == 1) ? 4 : 1; // 삼항연산자
+				y = (y < getHeight()) ? y + 10 : image.getWidth(this);
+
+			}
+		}
+		repaint(); // 한번 실행하면 다시 원래 이미지로 복귀
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		switch (sel) {
+		case 1:
+			image = Toolkit.getDefaultToolkit().getImage("src/images/smallMario.png");
+			// 고양이 이미지1 - 폴더는 개별로 다시 지정해야 됨
+			break;
+		case 2:
+			image = Toolkit.getDefaultToolkit().getImage("src/images/smallMario.png");
+			// 고양이 이미지2 - 폴더는 개별로 다시 지정해야 됨
+			break;
+
+		case 3:
+			image = Toolkit.getDefaultToolkit().getImage("src/images/smallMario.png");
+			// 고양이 이미지3 - 폴더는 개별로 다시 지정해야 됨
+			break;
+
+		case 4:
+			image = Toolkit.getDefaultToolkit().getImage("src/images/smallMario.png");
+			// 고양이 이미지4 - 폴더는 개별로 다시 지정해야 됨
+			break;
+		}
+
+		g.clearRect(0, 0, getWidth(), getHeight()); // 화면의 잔상 클리어하고 프로그램 시작
+
+		g.drawImage(image, x - 17, y - 25, x + 17, y + 25, 0, 0, 4096, 4096, this); // img의 (0,0)에서 image(4096,4096)부분을
+																					// 좌표에 따라 구현
+	}
+
+	@Override
+	public void keyReleased(java.awt.event.KeyEvent arg0) {
+	}
+
+	@Override
+	public void keyTyped(java.awt.event.KeyEvent arg0) {
+	}
+
+	
+
+}
+
