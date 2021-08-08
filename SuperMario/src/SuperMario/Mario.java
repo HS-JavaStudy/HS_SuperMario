2b622ceed64d5b6bb4a90907e40548c0e8e0d719
ujkkk <jean207@naver.com>
2021-08-08 18:18:19 +0900

    마리오 잔상없이 부드럽게 움직임 + 처음 위치가 이상

--- SuperMario/src/SuperMario/Mario.java
+++ SuperMario/src/SuperMario/Mario.java
@@ -11,8 +11,8 @@ public class Mario extends Thread{
 
 	private int delay = 20;
 	private long pretime;
-	private int cnt;
-	private int score;
+	private int cnt; // 프로그램 상 시간
+	private int score; // 점수
 	
 	boolean up;
 	private boolean down;
@@ -20,20 +20,21 @@ public class Mario extends Thread{
 	private boolean right;
 	private boolean isOver;
 	
-	private Image smallRightMario = new ImageIcon("src/images/smallRightMario.png").getImage();
-	private Image smallLefttMario = new ImageIcon("src/images/smallLefttMario.png").getImage();
+	private Image smallRightMario = new ImageIcon("src/images/smallRightMario.png").getImage(); // 이미지 객체 생성
+	private Image smallLeftMario = new ImageIcon("src/images/smallLeftMario.png").getImage();
 	private Image smallJumpRightMario = new ImageIcon("src/images/smallJumpRightMario.png").getImage();
-	private Image smallJumpLefttMario = new ImageIcon("src/images/smallJumpLefttMario.png").getImage();
+	private Image smallJumpLeftMario = new ImageIcon("src/images/smallJumpLeftMario%20.png").getImage();
+	private Image currentImage;
 
 	private int marioX, marioY;
 	private int marioWidth = 35;
 	private int marioHeight = 50;
-	private int marioSpeed = 10;
+	private int marioSpeed = 7;
 	private int marioHp = 30;
 
 
 
-	public void run() {
+	public void run() { //스레드를 시작할 떄 실행하는 코드. Mario.start()로 호출 가능
 
 		reset();
 		while (true) {
@@ -41,11 +42,12 @@ public class Mario extends Thread{
 				pretime = System.currentTimeMillis();
 				if (System.currentTimeMillis() - pretime < delay) {
 					try {
-						Thread.sleep(delay - System.currentTimeMillis() + pretime);
-						keyProcess();
+						Thread.sleep(delay - System.currentTimeMillis() + pretime); // 스레드가 잠을 자는 시간. 잠을 자는 동안 catch 블럭 실행한다
+						keyProcess(); // 키 프로세스 실행 
+						// 이외에 여러가지 프로세스 실행하도록 할 예정
 						//playerAttackProcess();
 					
-						cnt++;
+						cnt++; // 초 증가
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -58,8 +60,16 @@ public class Mario extends Thread{
 			}
 		}
 	}
-	
+	 public void reset() {
+	        isOver = false;
+	        cnt = 0;
+	        score = 0;
+	        marioX = 10;
+	        marioY = (MarioGame.SCREEN_HEIGHT - marioHeight) / 2;
+	    }
+	 
 	 private void keyProcess() {
+		 // 각 속성의 true, false 상태에 따라 결과 지정
 	        if (up && marioY - marioSpeed > 0) marioY -= marioSpeed;
 	        if (down && marioY + marioHeight + marioSpeed < MarioGame.SCREEN_HEIGHT) marioY += marioSpeed;
 	        if (left && marioX - marioSpeed > 0) marioX -= marioSpeed;
@@ -68,23 +78,26 @@ public class Mario extends Thread{
 	    }
 	 
 	  public void gameDraw(Graphics g) {
-	        playerDraw(g);
-	       // infoDraw(g);
+	        playerDraw(g); // MarioGame 클라스의 paint()함수 안에 있는 gameDraw() 함수
+	       
 	    }
 
-	   public void reset() {
-	        isOver = false;
-	        cnt = 0;
-	        score = 0;
-	        marioX = 10;
-	        marioY = (MarioGame.SCREEN_HEIGHT - marioHeight) / 2;
-	    }
+	  
 
-		public void playerDraw(Graphics g) {
-			g.drawImage(smallRightMario, marioX, marioY,
-					marioX + marioWidth, marioY + marioHeight,
-					0, 0, 1280, 1280,null);
+		public void playerDraw(Graphics g) { //마리오의 좌표에 따라 그 좌표에 그려지도록 구현
 			
+			// smallJumpRightMario
+			//smallLeftMario 
+			if(left) 
+				currentImage = smallLeftMario;
+			if(right)
+				currentImage = smallRightMario;
+			
+			 g.drawImage(currentImage  , marioX, marioY, //  해당 위치부터
+					marioX + marioWidth, marioY + marioHeight, // 이 위치 까지
+					0, 0, 1280, 1280, null); //마리오 사진의 (0,0) (1280, 1280)까지의 이미지를 그리기
+			
+		        
 		}
 	    
 	    public boolean isOver() {
--- SuperMario/src/SuperMario/MarioGame.java
+++ SuperMario/src/SuperMario/MarioGame.java
@@ -5,24 +5,27 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javax.swing.*;
+
 public class MarioGame extends JFrame{
 
-
+	private Image bufferImage;	//더블 버퍼링을 위한 전체 이미지를 담는 변수
+	private Graphics screenGraphic;
+	
 	public static final int SCREEN_WIDTH = 816;
     public static final int SCREEN_HEIGHT = 678;
     
     private Mario mario = new Mario();
+    
     public MarioGame() {
-    	
-    	setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    	// 게임을 출력할 창 지정
+    	setSize(SCREEN_WIDTH, SCREEN_HEIGHT); 
     	setTitle("Super Mario!");
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(null);
-        addKeyListener(new KeyListener());
-        gameStart();
+        addKeyListener(new KeyListener()); // 마우스 이벤트 추가
+        gameStart(); // 바로 게임 스타트
     }
     
     void init() {
@@ -33,32 +36,37 @@ public class MarioGame extends JFrame{
         Timer loadingTimer = new Timer();
         TimerTask loadingTask = new TimerTask() {
             @Override
-            public void run() {
-            	 mario.start();
-            }
+            public void run() { //스레드 코드로서 JVM에 의해 호출. 반드시 오버라이딩 하여 스레드 코드를 작성하여야 한다
+            	 mario.start(); //Mario mario 스레드 실행을 시작하도록 요청
+            } 
         };
         loadingTimer.schedule(loadingTask, 3000);
     }
  
 
-    public void paint(Graphics g) {
-        
-        screenDraw(g);
-        
+    public void paint(Graphics g) { // JVM에 의해 호출되는 그리기 함수. repaint() 요청시 이 함수 실행
+    	//더블 버퍼링 : 전체 화면에 맞는 이미지를 매순간 마다 생성해서 원하는 컴포넌트만을 출력하는 방법 
+    	// 그냥 이미지 객체로 게임을 만들면 버퍼링이 심해 더블 버퍼링 기법을 쓴다고 한다
+    	 bufferImage = createImage(SCREEN_WIDTH,SCREEN_HEIGHT); 
+         screenGraphic = bufferImage.getGraphics();
+         screenDraw(screenGraphic);
+         g.drawImage(bufferImage, 0, 0, null);
     }
 
     public void screenDraw(Graphics g) {
        
-        mario.gameDraw(g);
+        mario.gameDraw(g); // Mario 클래스의 gameDraw() 함수 호출 - 캐릭터, 몬스터 등 그리기
       
-        this.repaint();
+        this.repaint();	// 그래픽의 상태를 바꿨으니 화면에 구현되도록 rapaint 요청. 즉 종료될 때까지 매순간 다시 그린다
     }
 
+    //키 이벤트 Adapter를 상속받아 필요한 것만 구현
     class KeyListener extends KeyAdapter {
-        public void keyPressed(KeyEvent e) {
+    	
+        public void keyPressed(KeyEvent e) { // 키를 눌렀을 때
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_UP:
-                    mario.setUp(true);
+                    mario.setUp(true); // Mario.setUp == true;
                     break;
                 case KeyEvent.VK_DOWN:
                 	mario.setDown(true);
@@ -81,10 +89,10 @@ public class MarioGame extends JFrame{
             }
         }
 
-        public void keyReleased(KeyEvent e) {
+        public void keyReleased(KeyEvent e) { //키를 뗴었을 때
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_UP:
-                	mario.setUp(false);
+                	mario.setUp(false); // Mario.setUp == flase;
                     break;
                 case KeyEvent.VK_DOWN:
                 	mario.setDown(false);
@@ -96,7 +104,7 @@ public class MarioGame extends JFrame{
                 	mario.setRight(false);
                     break;
                 case KeyEvent.VK_SPACE:
-                	//mario.setShooting(false);
+                	
                     break;
             }
         }
