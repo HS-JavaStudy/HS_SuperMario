5385d077c560fabe95544354ae84c73f514065c2
ujkkk <jean207@naver.com>
2021-08-08 16:22:14 +0900

    마리오 부드럽게 움직이기 잔상 남음 + 사진 .pnpng로 변경

--- SuperMario/src/SuperMario/Mario.java
+++ SuperMario/src/SuperMario/Mario.java
@@ -1,106 +1,111 @@
 package SuperMario;
-
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-public class Mario extends JFrame implements KeyListener {
+public class Mario extends Thread{
 
-	Image image;
-	int x = 100, y = 100, sel = 1;
-
-	public Mario() {
-		super("마리오"); // 프로그램 명
-		setLayout(null); // 레이아웃 매니저 사용 안함
-		setResizable(false); // 창 크기 고정
-		setSize(816, 678);// 창 크기 지정
-		setBackground(Color.WHITE); // 창 배경 색상 지정
-		setVisible(true);
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램 닫힘 설정
-
-		addKeyListener(this); // frame에 KeyListener 장착
-
-		x = (int) getWidth() / 2; // frame의 너비 얻기
-		y = (int) getHeight() / 2;
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_NUMPAD6 || // 숫자키 NUMPAD
-				key == KeyEvent.VK_KP_RIGHT) {
-			sel =  1; // 오른쪽
-			x = (x < getWidth()) ? x + 10 : -image.getWidth(this);
-		} else {
-			int key1 = e.getKeyCode();
-			if (key1 == KeyEvent.VK_LEFT || key1 == KeyEvent.VK_NUMPAD4 || // 숫자키 NUMPAD
-					key1 == KeyEvent.VK_KP_LEFT) {
-				//sel = (sel == 1) ? 3 : 1; // 삼항연산자
-				sel =  2 ; //왼쪽
-				x = (x > 0) ? x - 10 : getWidth() + image.getWidth(this);
-			}
-			int key2 = e.getKeyCode();
-			if (key2 == KeyEvent.VK_UP || key2 == KeyEvent.VK_NUMPAD8 || // 숫자키 NUMPAD
-					key2 == KeyEvent.VK_KP_UP) {
-				sel = (sel == 1) ? 3 : 4; // 삼항연산자
-				//sel = 3;
-				y = (y > 0) ? y - 10 : getHeight() + image.getHeight(this);
-
-			}
-			int key3 = e.getKeyCode();
-			if (key3 == KeyEvent.VK_DOWN || key3 == KeyEvent.VK_NUMPAD2 || // 숫자키 NUMPAD
-					key3 == KeyEvent.VK_KP_DOWN) {
-				sel = (sel == 1) ? 4 : 1; // 삼항연산자
-				y = (y < getHeight()) ? y + 10 : image.getWidth(this);
-
-			}
-		}
-		repaint(); // 한번 실행하면 다시 원래 이미지로 복귀
-	}
-
-	@Override
-	public void paint(Graphics g) {
-		switch (sel) {
-		case 1: // 오른쪽
-			image = Toolkit.getDefaultToolkit().getImage("src/images/smallRightMario.jpg"); // 툴킷 클래스의 도움을 받아 이미지 클래스 생성
-			// 서있는 오른쪽 마리오
-			break;
-		case 2:  //왼쪽
-			image = Toolkit.getDefaultToolkit().getImage("src/images/smallLeftMario.jpg");
-			// 서있는 왼쪽 마리오
-			break;
-
-		case 3: //위 오른
-			image = Toolkit.getDefaultToolkit().getImage("src/images/smallJumpRightMario.jpg");
-			// 점프하는 마리오
-			break;
-
-		case 4: //위 왼쪽
-			image = Toolkit.getDefaultToolkit().getImage("src/images/smallJumpLeftMario.jpg");
-			// 고양이 이미지4 - 폴더는 개별로 다시 지정해야 됨
-			break;
-		}
-
-		g.clearRect(0, 0, getWidth(), getHeight()); // 화면의 잔상 클리어하고 프로그램 시작
-
-		g.drawImage(image, x - 17, y - 25, x + 17, y + 25, 0, 0, 1280, 1280, this); // img의 (0,0)에서 image(4096,4096)부분을
-																					// 좌표에 따라 구현
-	}
-
-	@Override
-	public void keyReleased(java.awt.event.KeyEvent arg0) {
-	}
-
-	@Override
-	public void keyTyped(java.awt.event.KeyEvent arg0) {
-	}
-
+	private int delay = 20;
+	private long pretime;
+	private int cnt;
+	private int score;
 	
+	boolean up;
+	private boolean down;
+	private boolean left;
+	private boolean right;
+	private boolean isOver;
+	
+	private Image smallRightMario = new ImageIcon("src/images/smallRightMario.png").getImage();
+	private Image smallLefttMario = new ImageIcon("src/images/smallLefttMario.png").getImage();
+	private Image smallJumpRightMario = new ImageIcon("src/images/smallJumpRightMario.png").getImage();
+	private Image smallJumpLefttMario = new ImageIcon("src/images/smallJumpLefttMario.png").getImage();
 
+	private int marioX, marioY;
+	private int marioWidth = 35;
+	private int marioHeight = 50;
+	private int marioSpeed = 10;
+	private int marioHp = 30;
+
+
+
+	public void run() {
+
+		reset();
+		while (true) {
+			while (!isOver) {
+				pretime = System.currentTimeMillis();
+				if (System.currentTimeMillis() - pretime < delay) {
+					try {
+						Thread.sleep(delay - System.currentTimeMillis() + pretime);
+						keyProcess();
+						//playerAttackProcess();
+					
+						cnt++;
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	 private void keyProcess() {
+	        if (up && marioY - marioSpeed > 0) marioY -= marioSpeed;
+	        if (down && marioY + marioHeight + marioSpeed < MarioGame.SCREEN_HEIGHT) marioY += marioSpeed;
+	        if (left && marioX - marioSpeed > 0) marioX -= marioSpeed;
+	        if (right && marioX + marioWidth + marioSpeed < MarioGame.SCREEN_WIDTH) marioX += marioSpeed;
+	      
+	    }
+	 
+	  public void gameDraw(Graphics g) {
+	        playerDraw(g);
+	       // infoDraw(g);
+	    }
+
+	   public void reset() {
+	        isOver = false;
+	        cnt = 0;
+	        score = 0;
+	        marioX = 10;
+	        marioY = (MarioGame.SCREEN_HEIGHT - marioHeight) / 2;
+	    }
+
+		public void playerDraw(Graphics g) {
+			g.drawImage(smallRightMario, marioX, marioY,
+					marioX + marioWidth, marioY + marioHeight,
+					0, 0, 1280, 1280,null);
+			
+		}
+	    
+	    public boolean isOver() {
+	        return isOver;
+	    }
+
+	    public void setUp(boolean up) {
+	        this.up = up;
+	    }
+
+	    public void setDown(boolean down) {
+	        this.down = down;
+	    }
+
+	    public void setLeft(boolean left) {
+	        this.left = left;
+	    }
+
+	    public void setRight(boolean right) {
+	        this.right = right;
+	    }
+
+	   
 }
-
--- SuperMario/src/SuperMario/MarioGame.java
+++ SuperMario/src/SuperMario/MarioGame.java
@@ -1,14 +1,19 @@
 package SuperMario;
 import java.awt.*;
+import javax.swing.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.*;
 public class MarioGame extends JFrame{
 
+
 	public static final int SCREEN_WIDTH = 816;
     public static final int SCREEN_HEIGHT = 678;
     
+    private Mario mario = new Mario();
     public MarioGame() {
     	
     	setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -16,34 +21,86 @@ public class MarioGame extends JFrame{
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(null);
+        addKeyListener(new KeyListener());
+        gameStart();
     }
     
-    public void paint(Graphics g) {
-
-        screenDraw(g);
+    void init() {
+    	
+    }
+    private void gameStart() {
        
+        Timer loadingTimer = new Timer();
+        TimerTask loadingTask = new TimerTask() {
+            @Override
+            public void run() {
+            	 mario.start();
+            }
+        };
+        loadingTimer.schedule(loadingTask, 3000);
+    }
+ 
+
+    public void paint(Graphics g) {
+        
+        screenDraw(g);
+        
     }
 
     public void screenDraw(Graphics g) {
-       // if (isMainScreen) { //¿¹½Ã
-        //    g.drawImage(mainScreen, 0, 0, null);
-       // }
-    
+       
+        mario.gameDraw(g);
+      
         this.repaint();
     }
 
     class KeyListener extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
-            
+                case KeyEvent.VK_UP:
+                    mario.setUp(true);
+                    break;
+                case KeyEvent.VK_DOWN:
+                	mario.setDown(true);
+                    break;
+                case KeyEvent.VK_LEFT:
+                	mario.setLeft(true);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                	mario.setRight(true);
+                    break;
+                case KeyEvent.VK_R:
+                    if (mario.isOver()) 
+                    break;
+                case KeyEvent.VK_ENTER:
+                    
+                    break;
+                case KeyEvent.VK_ESCAPE:
+                    System.exit(0);
+                    break;
             }
         }
 
         public void keyReleased(KeyEvent e) {
             switch (e.getKeyCode()) {
-               
+                case KeyEvent.VK_UP:
+                	mario.setUp(false);
+                    break;
+                case KeyEvent.VK_DOWN:
+                	mario.setDown(false);
+                    break;
+                case KeyEvent.VK_LEFT:
+                	mario.setLeft(false);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                	mario.setRight(false);
+                    break;
+                case KeyEvent.VK_SPACE:
+                	//mario.setShooting(false);
+                    break;
             }
         }
     }
-	
+
+    
 }
